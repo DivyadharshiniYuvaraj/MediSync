9f8409cb008c2acbf65e168ddc12c912015219ec
DivyadharshiniYuvaraj <ydivyadharshini29@gmail.com>
2026-03-12 11:36:24 +0530

    Updated Deployment

--- backend/src/main/java/com/hospitalmanagement/app/controller/PublicController.java
+++ backend/src/main/java/com/hospitalmanagement/app/controller/PublicController.java
@@ -1,15 +1,21 @@
 package com.hospitalmanagement.app.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.hospitalmanagement.app.entity.Role;
 import com.hospitalmanagement.app.entity.User;
-import com.hospitalmanagement.app.repository.UserRepository;
 import com.hospitalmanagement.app.repository.AvailableSlotRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.hospitalmanagement.app.repository.UserRepository;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/public")
